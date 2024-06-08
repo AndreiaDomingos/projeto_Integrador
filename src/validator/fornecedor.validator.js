38ee57ecfa00057ae509488dd2660ecaf916c484
Gabriel <passosc.gabriel@hotmail.com>
2024-06-08 11:57:45 -0300

    validatorUpdate

--- src/validator/fornecedor.validator.js
+++ src/validator/fornecedor.validator.js
@@ -1,19 +1,19 @@
-import { body, param } from 'express-validator';
+import { body, param } from 'express-validator'; //FEITO
 
 export const fornecedorValidator = [
   body('nome').isString().withMessage('Nome deve ser uma string'),
-  body('tempo_entrega').isNumeric().withMessage('Tempo de entrega deve ser um número'),
-  body('cnpj').isString().withMessage('CNPJ é obrigatória')
+  body('telefone').isString().withMessage('Telefone deve ser uma string'),
+  body('email').isString().withMessage('Email é obrigatório'), //conferir se tem que ser email .isString
 ]
 
 export const fornecedorUpdateValidator = [
-  param('id_fornecedor').isInt().withMessage('Id deve ser um número'),
+  param('id').isInt().withMessage('Id deve ser um número inteiro'),
   body('nome').isString().withMessage('Nome deve ser uma string'),
-  body('tempo_entrega').isNumeric().withMessage('Tempo de entrega deve ser um número'),
-  body('cnpj').isString().withMessage('CNPJ é obrigatória')
+  body('telefone').isString().withMessage('Telefone deve ser uma string'),
+  body('email').isString().withMessage('Email deve ser uma string')
 ]
 
 export const fornecedorIdValidator = [
-  param('id_fornecedor').isInt().withMessage('Id é obrigatório')
+  param('id').isInt().withMessage('Id é obrigatório')
 ]
 
